a00384ecddf6a80bbbb60c212ce31fa53edb08c1
Esenyi Sharon <105223828+Sharonusesopensource@users.noreply.github.com>
2022-07-11 16:25:41 +0000

    Challenge 1- I started of by creating an array of numbers(comprising of positive, negative and neutral integers) in conformity with the instruction above. Next, I declared my global variable and defined my function by calling the keyword(function), I assign a descriptive name to it and gave it a parameter- the array of numbers to sum.
    In the first section of my function, I employed the use of the 'for' loop to iterate over the data in the array and only then did I use the length property to iterate through the array numbers before utilize that same property to sum up the elements.
    In the final section of the function, I print this result in the console.
    
    To conclude, I execute my function by calling its name with its appointed parameter.
    
    Challenge 3- The task here was converting an array of temperatures in Celsius to Fahrenheit and returning a new array containing the converted temperatures.
    The first step I took was to create an array of temperatures in Celsius.
    The next thing I did was unpacking the values of this array individually, using the destructuring assignment syntax.
     Afterwards, I created a function with parameters, following the appropriate syntax.
    Following that, I have my math operation in compliance with the conversion formula given by Anne Marie Helmenstine, Ph.D., from the link provided in the question.
    Above, I individually applied the conversion formula to each of the elements in the array.
    Also, I used the .toFixed method to round up the results to whole number units.
    Then, I printed my result in the console alongside the unit for Fahrenheit and executed the function.
    Finally, I created an array of temperatures in Fahrenheit containing the result of the function.
    
    The result from both challenges were mathematically accurate and it was a welcomed test of my knowledge of javascript.

--- src/assignment.js
+++ src/assignment.js
@@ -1,3 +1,4 @@
+
 // ========================
 // DO NOT EDIT THIS BLOCK
 const assignment = {};
@@ -11,12 +12,30 @@ const assignment = {};
  * If you did Challenge - 1, remove the comment in the line just after this function
  * 
  * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
- */
+@returns number the sum of the numbers */
+ 
+// I started of by creating an array of numbers(comprising of positive, negative and neutral integers) in conformity with the instruction above.
+
+let arrayOfNumbers = [ 20, 30, -20, 200, -300, -65, 0, 2, 12, -17, -520, 662, 1234, -999, 240, 34, 77, -78, -32, -21, 30,0]
+// I declare my global variable.
+let add = 0
+//  I define my function by calling its keyword-function- and assign a descriptive name to it, giving it a parameter- the array of numbers to sum.
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  // In this section of my function, I employed the use of the 'for' loop to iterate over the data in the array.
+  for ( let s = 0; s < arrayOfNumbers.length; s++)
+  // Above, I used the length property to iterate through the array numbers and below, I implement that same property to sum up the elements.
+  {add = add + arrayOfNumbers[s]};
+  // Finally in my section, I print this result in my console.
+  console.log(add)
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+// To conclude, I execute my function by calling its name with its appointed parameter.
+sumOfNumbers(arrayOfNumbers)
+
+
+
+
+
+
 
 /**
  * Challenge - 2
@@ -46,10 +65,39 @@ function countEvenNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
-function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+// The first thing I did was to create an array of temperatures in celcius.
+let celsiusTemps = [ 20, 14, 32, 12]
+// My next step was to unpack the values of this array individually, using the destructuring assignment syntax. 
+let [ c1, c2, c3, c4] = celsiusTemps
+// Here, you can see me creating a function with the name required by the question and giving it parameters(the parameters here are the content of the array just with the names I assigned them after destructuring).
+function celsiusToFahrenheit(c1, c2, c3, c4) {
+  // Then, the math operation section following the conversion formula specified by Anne Marie Helmenstine, Ph.D., from the link provided in the question.
+  let Fah1 = ( c1 * 9/5) + 32
+  let Fah2 = ( c2 * 9/5) + 32
+  let Fah3 = ( c3 * 9/5) + 32
+  let Fah4 = ( c4 * 9/5) + 32
+  // Above, I individually applied the conversion formula to each of the elements in the array.
+  // I used the .toFixed method to round up the results to whole number units. 
+  let fah1 = Fah1.toFixed()
+  let fah2 = Fah2.toFixed()
+  let fah3 = Fah3.toFixed()
+  let fah4= Fah4.toFixed()
+  // Below, I printed my result in the console alongside the unit for fahrenheit.
+  
+  console.log (fah1 + "°F" )
+  console.log (fah2 + "°F"  )
+  console.log (fah3 + "°F" )
+  console.log (fah4 + "°F" )
+  
+  
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+// Finally, I execute the function.
+celsiusToFahrenheit(c1, c2, c3, c4)
+
+// The array of temperatures in Fahrenheit equivalent to the values in the array of temperatures in Celsius. 
+let fahrenheitTemps = [celsiusToFahrenheit(c1, c2, c3, c4)]
+// Displaying the content of my new array of temperatures in Fahrenheit.
+fahrenheitTemps.toString()
 
 
 // ========================
